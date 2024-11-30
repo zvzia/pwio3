a7d718865565a4d5301d6bd62fe05717645176ed
Daniel-Stankowski <daniostan909v@gmail.com>
2024-11-30 21:52:53 +0100

    First-read then do fix

--- src/main/java/pwio/Main.java
+++ src/main/java/pwio/Main.java
@@ -3,8 +3,10 @@ package pwio;
 import com.opencsv.CSVReader;
 
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.attribute.UserPrincipalLookupService;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -13,47 +15,43 @@ import java.util.stream.StreamSupport;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "D:\\Studia\\sem9\\pwio\\proj\\Airbnb_Open_Data.csv";
-        long start = System.nanoTime();
-        AtomicInteger counter = new AtomicInteger(0);
-        AtomicInteger failed = new AtomicInteger(0);
-        try (CSVReader csvReader = new CSVReader(new FileReader(filePath))) {
-            Stream<String[]> rowsStream = StreamSupport.stream(csvReader.spliterator(), true);
-
-            Map<String, Map<String, Long>> listingsCount = rowsStream.skip(1)
-                    .filter(row -> {
-                        try {
-                            counter.incrementAndGet();
-                            return Integer.parseInt(row[CsvConst.PRICE].replace("$", "").trim().replace(",", "").trim()) < 1000;
-                        } catch (NumberFormatException e) {
-                            failed.incrementAndGet();
-                            return false;
-                        }
-                    })
-                    .filter(row -> !Objects.equals(row[CsvConst.NEIGHBOURHOOD_GROUP], "") && !Objects.equals(row[CsvConst.NEIGHBOURHOOD], ""))
-                    .map(row -> new AbstractMap.SimpleEntry<>(row[CsvConst.NEIGHBOURHOOD_GROUP], row[CsvConst.NEIGHBOURHOOD]))
-                    .sorted((o1, o2) -> {
-                        int keyCompare = o2.getKey().compareTo(o1.getKey());
-                        return keyCompare == 0 ? o1.getValue().compareTo(o2.getValue()) : keyCompare;
-                    })
-                    .collect(Collectors.groupingBy(AbstractMap.SimpleEntry::getKey, Collectors.groupingBy(AbstractMap.SimpleEntry::getValue, LinkedHashMap::new, Collectors.counting())));
-
-            listingsCount.entrySet().stream().forEach(entry -> {
-                String group = entry.getKey();
-                Map<String, Long> hoods = entry.getValue();
-                hoods.entrySet().stream().forEach(entry2 -> {
-                            String hoodName = entry2.getKey();
-                            Long count = entry2.getValue();
-                            System.out.println("Group: " + group + ", hood: " + hoodName + ", count: " + count);
-                        }
-                );
-            });
-
+        String filePath = "D:\\pwio3\\Airbnb_Open_Data.csv";
+        List<String[]> rows = new ArrayList<>();
+        try {
+            CSVReader reader = new CSVReader(new FileReader(filePath));
+            reader.spliterator().forEachRemaining(rows::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        long start = System.nanoTime();
+        Stream<String[]> rowsStream = rows.parallelStream();
+        Map<String, Map<String, Long>> listingsCount = rowsStream.skip(1)
+                .filter(row -> {
+                    try {
+                        return Integer.parseInt(row[CsvConst.PRICE].replace("$", "").trim().replace(",", "").trim()) < 1000;
+                    } catch (NumberFormatException e) {
+                        return false;
+                    }
+                })
+                .filter(row -> !Objects.equals(row[CsvConst.NEIGHBOURHOOD_GROUP], "") && !Objects.equals(row[CsvConst.NEIGHBOURHOOD], ""))
+                .map(row -> new AbstractMap.SimpleEntry<>(row[CsvConst.NEIGHBOURHOOD_GROUP], row[CsvConst.NEIGHBOURHOOD]))
+                .sorted((o1, o2) -> {
+                    int keyCompare = o2.getKey().compareTo(o1.getKey());
+                    return keyCompare == 0 ? o1.getValue().compareTo(o2.getValue()) : keyCompare;
+                })
+                .collect(Collectors.groupingBy(AbstractMap.SimpleEntry::getKey, Collectors.groupingBy(AbstractMap.SimpleEntry::getValue, LinkedHashMap::new, Collectors.counting())));
         System.out.println((System.nanoTime() - start)/1000000);
-        System.out.println(counter.toString());
-        System.out.println(failed.toString());
+        listingsCount.entrySet().stream().forEach(entry -> {
+            String group = entry.getKey();
+            Map<String, Long> hoods = entry.getValue();
+            hoods.entrySet().stream().forEach(entry2 -> {
+                String hoodName = entry2.getKey();
+                Long count = entry2.getValue();
+                System.out.println("Group: " + group + ", hood: " + hoodName + ", count: " + count);
+            }
+            );
+        });
+
+
     }
 }
